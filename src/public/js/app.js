2092c8b5105654f4d6cee05b8d759cbc2a6423b0
임종민 <yjm6560@gmail.com>
2022-11-06 00:30:15 +0900

    #3 webRTC 및 미디어 관련 코드 제거

--- src/public/js/app.js
+++ src/public/js/app.js
@@ -5,96 +5,12 @@ document.getElementById("serverUrl").innerText = "";
 
 const socket = io.connect(serverUrl, { query: `uuid=${myUuid}`});
 
-const myFace = document.getElementById("myFace");
-const muteBtn = document.getElementById("mute");
-const cameraBtn = document.getElementById("camera");
-const cameraSelect = document.getElementById("cameras");
 const messageForm = document.getElementById("messageToSend");
 const chatList = document.getElementById("chats");
 const call = document.getElementById("call");
 const chatDiv = document.getElementById("chats");
 
-let myStream;
-let muted = false;
-let cameraOff = false;
 let roomName;
-let myPeerConnection;
-
-async function getCameras() {
-    try {
-        const devices = await navigator.mediaDevices.enumerateDevices();
-        const cameras = devices.filter((device) => device.kind === "videoinput");
-        const currentCamera = myStream.getVideoTracks()[0];
-        cameras.forEach((camera) => {
-            const option = document.createElement("option");
-            option.value = camera.deviceId;
-            option.innerText = camera.label;
-            if(currentCamera.label == camera.label) {
-                option.selected = true;
-            }
-            cameraSelect.appendChild(option);
-        });
-    } catch(e) {
-        console.log(e);
-    }
-}
-
-async function getMedia(deviceId) {
-    const initialConstraints = {
-        audio : true,
-        video : { facingMode : "user" }
-    };
-    const cameraConstraints = {
-        audio : true,
-        video : { deviceId : { exact : deviceId } }
-    };
-    try {
-        myStream = await navigator.mediaDevices.getUserMedia(
-            deviceId ? cameraConstraints : initialConstraints
-        );
-        myFace.srcObject = myStream;
-        if(!deviceId) {
-            await getCameras();
-        }
-        myStream.getVideoTracks().forEach((track) => (track.enabled = !cameraOff));
-        myStream.getAudioTracks().forEach((track) => (track.enabled = !muted));
-    } catch(e) {
-        console.log(e);
-    }
-}
-
-function handleMuteClick() {
-    myStream.getAudioTracks().forEach((track) => (track.enabled = !track.enabled));
-    if(!muted) {
-        muteBtn.innerText = "unmute";
-        muted = true;
-    } else {
-        muteBtn.innerText = "Mute";
-        muted = false;
-    }
-}
-
-function handleCameraClick() {
-    myStream.getVideoTracks().forEach((track) => (track.enabled = !track.enabled));
-    if(!cameraOff) {
-        cameraBtn.innerText = "Turn Camera On";
-        cameraOff = true;
-    } else {
-        cameraBtn.innerText = "Trun Camera Off";
-        cameraOff = false;
-    }
-}
-
-async function handleCameraChange() {
-    await getMedia(cameraSelect.value);
-    if(myPeerConnection) {
-        const videoTrack = myStream.getVideoTracks()[0];
-        const videoSender = myPeerConnection
-        .getSenders()
-        .find((sender) => sender.track.kind === "video");
-        videoSender.replaceTrack(videoTrack);
-    }
-}
 
 function handleSendMessage(event) {
     event.preventDefault();
@@ -108,9 +24,6 @@ function handleSendMessage(event) {
     input.value = "";
 }
 
-muteBtn.addEventListener("click", handleMuteClick);
-cameraBtn.addEventListener("click", handleCameraClick);
-cameraSelect.addEventListener("click", handleCameraChange);
 messageForm.addEventListener("submit", handleSendMessage);
 
 // Welcome For  (join a room)
@@ -122,8 +35,6 @@ call.hidden = true;
 async function initCall() {
     welcome.hidden = true;
     call.hidden = false;
-    await getMedia();
-    makeConnection();
 }
 
 async function handleWelcomeSubmit(event) {
@@ -140,31 +51,7 @@ welcomeForm.addEventListener("submit", handleWelcomeSubmit);
 // Socket Code
 
 socket.on("welcome", async () => {
-    console.log("made data channel");
-    const offer = await myPeerConnection.createOffer();
-    console.log(offer);
-    myPeerConnection.setLocalDescription(offer);
-    console.log("sent the offer");
-    socket.emit("offer", offer, roomName);
-});
-
-socket.on("offer", async (offer) => {
-    console.log("received the offer");
-    myPeerConnection.setRemoteDescription(offer);
-    const answer = await myPeerConnection.createAnswer();
-    myPeerConnection.setLocalDescription(answer);
-    socket.emit("answer", answer, roomName);
-    console.log("sent the answer");
-});
-
-socket.on("answer", answer => {
-    console.log("received the answer");
-    myPeerConnection.setRemoteDescription(answer);
-});
-
-socket.on("ice", ice => {
-    console.log("received candidate");
-    myPeerConnection.addIceCandidate(ice);
+    console.log("welcome!");
 });
 
 socket.on("new_message", message => {
@@ -180,41 +67,3 @@ socket.on("new_message", message => {
     chatList.appendChild(li);
     chatDiv.scrollTop = chatDiv.scrollHeight;
 });
-
-socket.on("disconnected", handleRemoveStream);
-
-// RTC Code
-
-function makeConnection() {
-    myPeerConnection = new RTCPeerConnection({
-        iceServers: [
-            {
-                urls: [
-                    "stun:stun.l.google.com:19302",
-                    "stun:stun1.l.google.com:19302",
-                    "stun:stun2.l.google.com:19302",
-                    "stun:stun3.l.google.com:19302",
-                    "stun:stun4.l.google.com:19302",
-                ]
-            }
-        ]
-    });
-    myPeerConnection.addEventListener("icecandidate", handleIce);
-    myPeerConnection.addEventListener("addstream", handleAddStream);
-    myStream.getTracks()
-    .forEach(track => myPeerConnection.addTrack(track, myStream));
-}
-
-function handleIce(data) {
-    console.log("sent candidate");
-    socket.emit("ice", data.candidate, roomName);
-}
-
-function handleAddStream(data) {
-    const peerFace = document.getElementById("peerFace");
-    peerFace.srcObject = data.stream;
-}
-
-function handleRemoveStream() {
-    document.getElementById("peerFace").srcObject = null;
-}
